e2db503590da401bf3e1f17dcde6f7fe816acc5c
Chloe Nixon <93556823+ChloeNixon@users.noreply.github.com>
2024-07-24 20:58:51 -0500

    registration form of site

--- src/Temp_Hero.tsx
+++ src/Temp_Hero.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import {Image, Flex, Text, Button} from "@mantine/core";
-import { useViewportSize } from "@mantine/hooks";
+import {Image, Flex, Text} from "@mantine/core";
+import { Button } from '@mantine/core';
+import {useViewportSize} from "@mantine/hooks";
 import VHLogo from "./assets/VHlogo_racing.svg";
 import ducky_red from "./assets/ducky_red.svg";
 import ducky_blue from "./assets/ducky_blue.svg";
@@ -12,7 +13,7 @@ function Temp_Hero() {
     return (
         <>
             <Flex
-                top="7vh"
+                top="5vh"
                 pos="relative"
                 w="100%"
                 direction={"column"}
@@ -21,29 +22,33 @@ function Temp_Hero() {
                 ta="center"
                 gap="sm"
             >
-                <Image w="60vh" src={VHLogo} alt="VandyHacks Logo"/>
-                <Text className="hero_text"
-                >Vanderbilt's student-run hackathon</Text>
-                <Text className="hero_date">September 27-29th</Text>
+                <Image w="70vh" src={VHLogo} alt="VandyHacks Logo"/>
 
-                <Button color="gray" radius="md" size="xl">
+                <div className="m_green">
+                    <Image pos="relative" w="17vh" src={ducky_green} alt="Janice"/>
+                </div>
+                <div className="m_red">
+                    <Image pos="relative" w="17vh" src={ducky_red} alt="Jennifer"/>
+                </div>
+                <div className="m_bumping">
+                    <div className="m_blue">
+                        <Image pos="relative" w="17vh" src={ducky_blue} alt="Jessica"/>
+                    </div>
+                </div>
+
+                <Text className="hero_text">
+                    Vanderbilt's student-run hackathon
+                </Text>
+                <Text className="hero_date">September 27-29th</Text>
+                <Button className="register">
                     Register
                 </Button>
+                <Text className="sub_text">
+                    If you want more information or are interested in sponsoring us,
+                    please contact us at <br></br> chloe.p.nixon@vanderbilt.edu and aryan.garg@vanderbilt.edu
+                </Text>
 
             </Flex>
-            <div className="m_green">
-                <Image pos="relative" top="-50vh" w="18vh" src={ducky_green} alt="Janice"/>
-            </div>
-            <div className="m_red">
-                <Image pos="relative" top="-53vh" w="18vh" src={ducky_red} alt="Jennifer"/>
-            </div>
-            <div className="m_bumping">
-            <div className="m_blue">
-                <Image pos="relative" top="-56vh" w="18vh" src={ducky_blue} alt="Jessica"/>
-            </div>
-            </div>
-
-
         </>
     );
 }
