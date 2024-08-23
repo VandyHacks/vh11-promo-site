398e504844c0454a307a8d426b35744a9a2428ed
kyleburgess <kylie.m.burgess@vanderbilt.edu>
2024-08-23 10:16:55 -0500

    Logos grid currently not working on some browsers

--- src/sponsors/LogosGrid.tsx
+++ src/sponsors/LogosGrid.tsx
@@ -1,31 +1,30 @@
 import React from "react";
 import {Image, Anchor, Grid} from "@mantine/core";
-// eslint-disable-next-line
 import {useViewportSize} from "@mantine/hooks";
 import "./Sponsors.css";
 import phosphorus from "../assets/sponsors_logos/phosphorus.svg"
 import capitalone from "../assets/sponsors_logos/Capital_One_logo.svg";
-import VUisis from "../assets/sponsors_logos/ISIS-logo.jpg";
+import vuIsis from "../assets/sponsors_logos/ISIS-logo.jpg";
 import wolfram from "../assets/sponsors_logos/Wolfram_Alpha_2022.svg";
 
 const sponsors = [
     {
-        name: capitalone,
+        logo: capitalone,
         link: "https://www.capitalonecareers.com/internship-programs",
         span: 3,
     },
     {
-        name: phosphorus,
+        logo: phosphorus,
         link: "https://phosphorus.io/",
         span: 3,
     },
     {
-        name: wolfram,
+        logo: wolfram,
         link: "https://www.wolframalpha.com/",
         span: 3,
     },
     {
-        name: VUisis,
+        logo: vuIsis,
         link: "https://www.isis.vanderbilt.edu/",
         span: 3,
     },
@@ -43,7 +42,7 @@ function LogoMap() {
                         <Grid.Col span={"auto"}>
                             <Image
                                 w="50vh"
-                                src={sponsor.name}
+                                src={sponsor.logo}
                                 alt="sponsor logo"
                             />
                         </Grid.Col>
